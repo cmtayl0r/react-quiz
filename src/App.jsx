b14b824b68fc62809ee409f9a374fbf9e8168609
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-09-18 19:50:02 +0200

    add init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>React Quiz</h1>
+    </div>
+  );
+}
+
+export default App;
