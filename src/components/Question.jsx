28fac157210cf42fe48d87fa7329bbdc5bd54355
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-09-22 11:56:19 +0200

    add Options component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,15 +1,11 @@
+import Options from "./Options";
+
 function Question({ question }) {
   console.log(question);
   return (
     <div>
       <h4>{question.question}</h4>
-      <div className="options">
-        {question.options.map((option, index) => (
-          <button className="btn btn-option" key={index}>
-            {option}
-          </button>
-        ))}
-      </div>
+      <Options options={question.options} />
     </div>
   );
 }
